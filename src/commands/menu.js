67448419d762681ca81344d470c4d700d26ab1a7
LORD PREMO <cypherx599@gmail.com>
2026-02-28 23:06:04 +0300

    Delete Procfile

--- src/commands/menu.js
+++ src/commands/menu.js
@@ -15,30 +15,40 @@ async function menu(sock, msg, info, reply) {
   const barLength = 10;
   const filled = Math.round((ramPercent / 100) * barLength);
   const bar = '█'.repeat(filled) + '░'.repeat(barLength - filled);
-
   const uptime = formatUptime(process.uptime());
 
   const menuText = `
 ┏▣ ◈ *${info.botName}* ◈
-┃ *ᴏᴡɴᴇʀ* : Not Set!
+┃ *ᴏᴡɴᴇʀ* : ${info.ownerNumber || 'Not Set'}
 ┃ *ᴘʀᴇғɪx* : [ ${info.prefix} ]
 ┃ *ʜᴏsᴛ* : Render
-┃ *ᴘʟᴜɢɪɴs* : Offline Core
 ┃ *ᴍᴏᴅᴇ* : Private
 ┃ *ᴠᴇʀsɪᴏɴ* : 1.0.0
-┃ *sᴘᴇᴇᴅ* : ~ ms
 ┃ *ᴜᴘᴛɪᴍᴇ* : ${uptime}
 ┃ *ᴜsᴀɢᴇ* : ${used.toFixed(1)} MB of ${total.toFixed(0)} MB
 ┃ *ʀᴀᴍ:* [${bar}] ${ramPercent}%
 ┗▣ 
 
-┏▣ ◈ *AI MENU* ◈
-│➽ analyze
-│➽ code
-│➽ generate
-│➽ programming
-│➽ summarize
-│➽ teach
+┏▣ ◈ *OWNER MENU* ◈
+│➽ owner
+│➽ restart
+│➽ bc
+│➽ mode
+┗▣ 
+
+┏▣ ◈ *PROTECT MENU* ◈
+│➽ antilink
+│➽ antibadword
+│➽ antidelete
+│➽ autotyping
+│➽ addbadword
+│➽ delbadword
+│➽ listbadword
+┗▣ 
+
+┏▣ ◈ *GROUP MENU* ◈
+│➽ tagall
+│➽ welcome
 ┗▣ 
 
 ┏▣ ◈ *FUN MENU* ◈
@@ -48,9 +58,10 @@ async function menu(sock, msg, info, reply) {
 │➽ trivia
 ┗▣ 
 
-┏▣ ◈ *GROUP MENU* ◈
-│➽ tagall
-│➽ welcome
+┏▣ ◈ *TOOLS MENU* ◈
+│➽ calculate
+│➽ fliptext
+│➽ say
 ┗▣ 
 
 ┏▣ ◈ *OTHER MENU* ◈
@@ -58,17 +69,8 @@ async function menu(sock, msg, info, reply) {
 │➽ ping
 │➽ runtime
 │➽ time
-┗▣ 
-
-┏▣ ◈ *SETTINGS MENU* ◈
 │➽ getsettings
 │➽ resetsetting
-┗▣ 
-
-┏▣ ◈ *TOOLS MENU* ◈
-│➽ calculate
-│➽ fliptext
-│➽ say
 ┗▣ 
 `.trim();
 
